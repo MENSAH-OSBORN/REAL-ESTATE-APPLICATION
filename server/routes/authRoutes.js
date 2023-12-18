32f9c024e5517fff2d98322a7dc7c2f17d5fc7d2
richmond-andoh <richmondandoh00@gmail.com>
2023-12-18 20:46:28 +0000

    add user authentication routes

---
+++ server/routes/authRoutes.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const router = express.Router();
+const authController = require("../controllers/authController");
+
+router.route("/register").post(authController.register);
+router.route("/login").post(authController.login);
+router.route("/forgot-password").post(authController.forgotPassword);
+router.route("/reset-password/:token").post(authController.resetPassword);
+
+module.exports = router;
